ab6907e3a00dbe49ce84f517c9a83ab482116119
MonkeyPython <jl.guti.ramos@gmail.com>
2019-08-13 11:37:17 +0200

    Challenge 2 - tested dockers

--- challenges/Docker/MongoDB/docker-entrypoint.sh
+++ challenges/Docker/MongoDB/docker-entrypoint.sh
@@ -2,6 +2,6 @@
 
 /usr/bin/mongod --fork --logpath /var/log/mongodb.log
 
-/usr/bin/mongoimport --db servicesdb --collection restaurants --file /data/files/restaurant.json
+/usr/bin/mongoimport --db servicesDB --collection restaurants --file /data/files/restaurant.json
 
 tail -1000f /var/log/mongodb.log
